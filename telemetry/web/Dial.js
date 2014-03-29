5a6e3954120fb2cc4370e088e6623b9106941e4f
Kevin Hoyt <kevin.hoyt@kaazing.com>
2014-03-29 15:34:22 -0600

    Ranges added to dial
    Colored ranges for arbitrary low (green), medium (yellow) and high (red).  Took some trigonometry and tricky SVG masking to get working.

--- telemetry/web/Dial.js
+++ telemetry/web/Dial.js
@@ -28,16 +28,28 @@ function Dial( selector, label, minimum, maximum )
     defs = document.createElementNS( SVG_NAMESPACE, "defs" );
     result.document.appendChild( defs );
 
+    // Low points
+    hypotenuse = ( result.container.clientWidth / 2 ) - 1;
+    startAdjacent = Math.sin( 60 * ( Math.PI / 180 ) ) * hypotenuse;
+    startOpposite = Math.cos( 60 * ( Math.PI / 180 ) ) * hypotenuse;
+
+    mediumAdjacent = Math.sin( 50 * ( Math.PI / 180 ) ) * hypotenuse;
+    mediumOpposite = Math.cos( 50 * ( Math.PI / 180 ) ) * hypotenuse;
+
     // Clipping for low range
     clipping = document.createElementNS( SVG_NAMESPACE, "clipPath" );
     clipping.setAttribute( "id", "low" );
     defs.appendChild( clipping );
 
-    element = document.createElementNS( SVG_NAMESPACE, "rect" );
-    element.setAttribute( "x", 0 );
-    element.setAttribute( "y", result.container.clientHeight / 2 );
-    element.setAttribute( "width", result.container.clientWidth / 2 );
-    element.setAttribute( "height", result.container.clientHeight / 2 );
+    element = document.createElementNS( SVG_NAMESPACE, "path" );
+    element.setAttribute( "d",
+        "M" + hypotenuse + ", " + hypotenuse + " " +
+        "l" + ( 0 - startAdjacent ) + ", " + startOpposite + " " +
+        "L0, " + ( hypotenuse + startOpposite ) + " " +
+        "L0, 0 " +
+        "L" + ( hypotenuse - mediumAdjacent ) + ", " + ( hypotenuse - mediumOpposite ) + " " +
+        "L" + hypotenuse + ", " + hypotenuse
+    );
     element.setAttribute( "fill", "red" );
     clipping.appendChild( element );
 
@@ -46,11 +58,15 @@ function Dial( selector, label, minimum, maximum )
     clipping.setAttribute( "id", "medium" );
     defs.appendChild( clipping );
 
-    element = document.createElementNS( SVG_NAMESPACE, "rect" );
-    element.setAttribute( "x", 0 );
-    element.setAttribute( "y", 0 );
-    element.setAttribute( "width", result.container.clientWidth );
-    element.setAttribute( "height", result.container.clientHeight / 2 );
+    element = document.createElementNS( SVG_NAMESPACE, "path" );
+    element.setAttribute( "d",
+        "M" + hypotenuse + ", " + hypotenuse + " " +
+        "L" + ( hypotenuse - mediumAdjacent ) + ", " + ( hypotenuse - mediumOpposite ) + " " +
+        "L" + ( hypotenuse - mediumAdjacent ) + ", 0 " +
+        "L" + ( hypotenuse + mediumAdjacent ) + ", 0 " +
+        "L" + ( hypotenuse + mediumAdjacent ) + ", " + ( hypotenuse - mediumOpposite ) + " " +
+        "L" + hypotenuse + ", " + hypotenuse
+    );
     element.setAttribute( "fill", "red" );
     clipping.appendChild( element );
 
@@ -59,11 +75,15 @@ function Dial( selector, label, minimum, maximum )
     clipping.setAttribute( "id", "high" );
     defs.appendChild( clipping );
 
-    element = document.createElementNS( SVG_NAMESPACE, "rect" );
-    element.setAttribute( "x", result.container.clientWidth / 2 );
-    element.setAttribute( "y", result.container.clientHeight / 2 );
-    element.setAttribute( "width", result.container.clientWidth / 2 );
-    element.setAttribute( "height", result.container.clientHeight / 2 );
+    element = document.createElementNS( SVG_NAMESPACE, "path" );
+    element.setAttribute( "d",
+        "M" + hypotenuse + ", " + hypotenuse + " " +
+        "L" + ( hypotenuse + mediumAdjacent ) + ", " + ( hypotenuse - mediumOpposite ) + " " +
+        "L" + result.container.clientWidth + ", 0 " +
+        "L" + result.container.clientWidth + ", " + ( hypotenuse + startOpposite ) + " " +
+        "L" + ( hypotenuse + startAdjacent ) + ", " + ( hypotenuse + startOpposite ) + " " +
+        "L" + hypotenuse + ", " + hypotenuse
+    );
     element.setAttribute( "fill", "red" );
     clipping.appendChild( element );
 
@@ -76,6 +96,42 @@ function Dial( selector, label, minimum, maximum )
     element.setAttribute( "stroke", "white" );
     result.document.appendChild( element );
 
+    // Minimum (0) position
+    var holder = document.createElementNS( SVG_NAMESPACE, "g" );
+    holder.setAttribute( "transform", "rotate( " +
+        "-120 " +
+        ( result.container.clientWidth / 2 ) + " " +
+        ( result.container.clientHeight / 2 ) + " )"
+    );
+    result.document.appendChild( holder );
+
+    element = document.createElementNS( SVG_NAMESPACE, "polyline" );
+    element.setAttribute( "points",
+        ( result.container.clientWidth / 2 ) + ", 1 " +
+        ( result.container.clientWidth / 2 ) + ", " + 15
+    );
+    element.setAttribute( "stroke", "white" );
+    element.setAttribute( "stroke-width", 1 );
+    holder.appendChild( element );
+
+    // Maximum (100) position
+    var holder = document.createElementNS( SVG_NAMESPACE, "g" );
+    holder.setAttribute( "transform", "rotate( " +
+        "120 " +
+        ( result.container.clientWidth / 2 ) + " " +
+        ( result.container.clientHeight / 2 ) + " )"
+    );
+    result.document.appendChild( holder );
+
+    element = document.createElementNS( SVG_NAMESPACE, "polyline" );
+    element.setAttribute( "points",
+        ( result.container.clientWidth / 2 ) + ", 1 " +
+        ( result.container.clientWidth / 2 ) + ", " + 15
+    );
+    element.setAttribute( "stroke", "white" );
+    element.setAttribute( "stroke-width", 1 );
+    holder.appendChild( element );
+
     // Low dial
     element = document.createElementNS( SVG_NAMESPACE, "circle" );
     element.setAttribute( "clip-path", "url( #low )" );
@@ -83,7 +139,7 @@ function Dial( selector, label, minimum, maximum )
     element.setAttribute( "cy", result.container.clientHeight / 2 );
     element.setAttribute( "r", ( result.container.clientWidth / 2 ) - 3 );
     element.setAttribute( "fill", "none" );
-    element.setAttribute( "stroke", "white" );
+    element.setAttribute( "stroke", "green" );
     element.setAttribute( "stroke-width", 3 );
     result.document.appendChild( element );
 
@@ -94,7 +150,7 @@ function Dial( selector, label, minimum, maximum )
     element.setAttribute( "cy", result.container.clientHeight / 2 );
     element.setAttribute( "r", ( result.container.clientWidth / 2 ) - 3 );
     element.setAttribute( "fill", "none" );
-    element.setAttribute( "stroke", "green" );
+    element.setAttribute( "stroke", "yellow" );
     element.setAttribute( "stroke-width", 3 );
     result.document.appendChild( element );
 
@@ -130,42 +186,6 @@ function Dial( selector, label, minimum, maximum )
     result.current.setAttribute( "font-size", FONT_SIZE );
     result.current.setAttribute( "font-family", FONT_FAMILY );
     result.document.appendChild( result.current );
-
-    // Minimum (0) position
-    var holder = document.createElementNS( SVG_NAMESPACE, "g" );
-    holder.setAttribute( "transform", "rotate( " +
-        "-120 " +
-        ( result.container.clientWidth / 2 ) + " " +
-        ( result.container.clientHeight / 2 ) + " )"
-    );
-    result.document.appendChild( holder );
-
-    element = document.createElementNS( SVG_NAMESPACE, "polyline" );
-    element.setAttribute( "points",
-        ( result.container.clientWidth / 2 ) + ", 1 " +
-        ( result.container.clientWidth / 2 ) + ", " + ( result.container.clientHeight / 2 )
-    );
-    element.setAttribute( "stroke", "blue" );
-    element.setAttribute( "stroke-width", 3 );
-    holder.appendChild( element );
-
-    // Maximum (100) position
-    var holder = document.createElementNS( SVG_NAMESPACE, "g" );
-    holder.setAttribute( "transform", "rotate( " +
-        "120 " +
-        ( result.container.clientWidth / 2 ) + " " +
-        ( result.container.clientHeight / 2 ) + " )"
-    );
-    result.document.appendChild( holder );
-
-    element = document.createElementNS( SVG_NAMESPACE, "polyline" );
-    element.setAttribute( "points",
-        ( result.container.clientWidth / 2 ) + ", 1 " +
-            ( result.container.clientWidth / 2 ) + ", " + ( result.container.clientHeight / 2 )
-    );
-    element.setAttribute( "stroke", "blue" );
-    element.setAttribute( "stroke-width", 3 );
-    holder.appendChild( element );
 
     // Needle
     result.needle = document.createElementNS( SVG_NAMESPACE, "g" );
