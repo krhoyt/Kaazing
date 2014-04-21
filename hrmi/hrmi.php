bc833632d7d760db5c6090b18dd320e58906638d
Kevin Hoyt <kevin.hoyt@kaazing.com>
2014-04-21 17:02:40 -0600

    Working on user interface formatting
    Started formatting of user interface.  Tested briefly on iPad.

--- hrmi/hrmi.php
+++ hrmi/hrmi.php
@@ -5,7 +5,7 @@ require_once( "Stomp.php" );
 
 // Broker URI
 $BROKER_URI = "tcp://ec2-54-211-72-62.compute-1.amazonaws.com:61613";
-$TOPIC = "/topic/heart";
+$TOPIC = "/topic/hrmi";
 
 // Connection
 $conn = new Stomp( $BROKER_URI );
