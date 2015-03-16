d077023361bdd72d83f03292d0524f6a6bae5d44
krhoyt <parkerkrhoyt@gmail.com>
2015-03-16 09:40:02 -0600

    Generate sine wave at Arduino.
    Arduino code now generates three different sine wave formations, at
    randomly different points.  Values are -1 to 1.  Easy to chart.

--- iot/arduino/Counter/Counter.ino
+++ iot/arduino/Counter/Counter.ino
@@ -1,23 +1,41 @@
-// Counter
-int count = 0;
+// #define PI 3.141592653589793
+
+// Counters
+int comfort;
+int index;
+int usage;
 
 // Setup
 // Configure serial
 void setup() {
+  comfort = random( 360 );
+  index = random( 360 );
+  usage = random( 360 );
+  
   Serial.begin( 9600 );
 }
 
 // Loop
 // Print counter
-void loop() {
-  // Increment counter
-  count = count + 1;
-  
+void loop() {  
   // Print record
   Serial.print( "#" );
-  Serial.println( count );
+  Serial.print( sin( toRadians( usage ) ) );
+  Serial.print( "," );
+  Serial.print( sin( toRadians( index ) ) );
+  Serial.print( "," );
+  Serial.println( sin( toRadians( comfort ) ) );
   
-  // Wait a second
-  delay( 1000 );
+  // Increment counters
+  index = index + 1;  
+  comfort = comfort + 1;  
+  usage = usage + 1;    
+  
+  // Give serial time to
+  delay( 100 );
+}
+
+float toRadians( int deg )  {
+  return deg * ( PI / 180 );
 }
 
