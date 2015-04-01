a6fc1cf31f55fbc5928fd7564d409948f1fde257
krhoyt <parkerkrhoyt@gmail.com>
2015-04-01 13:46:12 -0600

    Animated gauges based on message data.
    Dashboard gauges now animate to correct position.

--- iot/cars/java/src/Cars.java
+++ iot/cars/java/src/Cars.java
@@ -4,7 +4,6 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.io.InputStream;
 import java.io.PrintWriter;
 import java.io.StringWriter;
 import java.io.UnsupportedEncodingException;
